6683fd4af5916f1f994cfc07303dd2598c1f9489
Jonwilm <jparedes84@gmail.com>
2019-09-24 09:37:33 -0400

    Ajustes

--- memoria/main.js
+++ memoria/main.js
@@ -108,7 +108,7 @@
           frag += '<div class="card" data-id="'+ v.id +'"><div class="inside">\
           <div class="front"><img src="'+ v.img +'"\
           alt="'+ v.name +'" /></div>\
-          <div class="back"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/codepen-logo.png"\
+          <div class="back"><img src="images/codepen-logo.png"\
           alt="Codepen" /></div></div>\
           </div>';
         });
@@ -119,62 +119,62 @@
     var cards = [
     {
       name: "php",
-      img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/php-logo_1.png",
+      img: "images/php-logo_1.png",
       id: 1,
     },
     {
       name: "css3",
-      img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/css3-logo.png",
+      img: "images/css3-logo.png",
       id: 2
     },
     {
       name: "html5",
-      img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/html5-logo.png",
+      img: "images/html5-logo.png",
       id: 3
     },
     {
       name: "jquery",
-      img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/jquery-logo.png",
+      img: "images/jquery-logo.png",
       id: 4
     }, 
     {
       name: "javascript",
-      img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/js-logo.png",
+      img: "images/js-logo.png",
       id: 5
     },
     {
       name: "node",
-      img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/nodejs-logo.png",
+      img: "images/nodejs-logo.png",
       id: 6
     },
     {
       name: "photoshop",
-      img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/photoshop-logo.png",
+      img: "images/photoshop-logo.png",
       id: 7
     },
     {
       name: "python",
-      img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/python-logo.png",
+      img: "images/python-logo.png",
       id: 8
     },
     {
       name: "rails",
-      img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/rails-logo.png",
+      img: "images/rails-logo.png",
       id: 9
     },
     {
       name: "sass",
-      img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/sass-logo.png",
+      img: "images/sass-logo.png",
       id: 10
     },
     {
       name: "sublime",
-      img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/sublime-logo.png",
+      img: "images/sublime-logo.png",
       id: 11
     },
     {
       name: "wordpress",
-      img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/wordpress-logo.png",
+      img: "images/wordpress-logo.png",
       id: 12
     },
     ];
